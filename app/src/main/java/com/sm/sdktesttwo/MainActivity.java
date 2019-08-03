9747fa2f1a44263cc4b762d8f7b731311057ca82
ANDROID-DEV-RASEL <rasel@shurjomukhi.com.bd>
2019-08-03 11:18:00 +0600

    Initial commit

---
+++ app/src/main/java/com/sm/sdktesttwo/MainActivity.java
@@ -0,0 +1,14 @@
+package com.sm.sdktesttwo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ rsllibrary/src/main/java/com/sm/rsllibrary/GetString.java
@@ -0,0 +1,7 @@
+package com.sm.rsllibrary;
+
+public class GetString {
+    public static String getSampleTest(){
+        return "how are u mr";
+    }
+}
